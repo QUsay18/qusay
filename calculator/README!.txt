ccddcec772d235d2b6d73a87a87c5b5c4ff1907d
QUsay18 <alkhaldq245@gmail.com>
2026-04-04 10:22:55 +0300

    calculator

---
+++ calculator/README!.txt
@@ -0,0 +1,11 @@
+to use this to tool you must first
+install python3 on you Terminl by:
+ sudo apt install python3
+and then type python3  on you Terminl
+and then drag you the python file 
+intro you Terminl
+and that it
+
+
+
+
---
+++ calculator/python/calculator.py
@@ -0,0 +1,17 @@
+
+num1 = int(input("pls enter number: "))
+print(num1)
+totale = input()
+print(totale) 
+num2 = int(input("enter seconed number: "))
+print(num2)
+
+print("the totale: ")
+if totale == "+":
+    print(num1 + num2)
+elif totale == "-":
+    print(num1 - num2)
+elif totale == "x":
+    print(num1 * num2)
+elif totale == "/":
+    print(num1 / num2)
